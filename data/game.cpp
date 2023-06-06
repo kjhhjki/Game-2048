83a3bb888073809d16ef0e5958ed0202b17cb1e3
Li Jiajun <111627930+kjhhjki@users.noreply.github.com>
2023-06-06 17:02:04 +0800

    Update game.cpp

--- data/game.cpp
+++ data/game.cpp
@@ -56,6 +56,7 @@ void Game::init()
     flush();
     ct = get_current_time();
     timer.timer_start();
+    rand_init();
     print();
     threads.push_back(std::thread(&Game::timing, this, std::move(this->prom.get_future())));
     this->play();
